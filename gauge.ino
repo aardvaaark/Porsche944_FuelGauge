d626eb1ad861f695d1152e0c250921935545a30f
Aardvaaark <aardvaaark@github.com>
2020-10-20 13:39:32 -0400

    formatting

--- gauge.ino
+++ gauge.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal.h>
 #include <LcdBarGraph.h>
- 
+
 const byte lcdNumCols = 16;
 double rawSendingUnitValue;
 int rawPercentage;
@@ -15,77 +15,83 @@ int smoothedPercentage;
 // 20 is solid for testing...in production this will likely be much higher, 100 or more perhaps
 const int numReadings = 20;
 
-int readings[numReadings];      // the readings from the analog input
-int readIndex = 0;              // the index of the current reading
-int total = 0;                  // the running total
-int average = 0;                // the average  
- 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // -- creating LCD instance
-LcdBarGraph lbg(&lcd, lcdNumCols, 0, 1);// -- creating bargraph instance, format is (&lcd, lcdNumCols, start X, start Y). So (&lcd, 16, 0, 1) would set the bargraph length to 16 columns and start the bargraph at column 0 on row 1.
- 
-void setup(){
-  Serial.begin(9600);
-  
-  // initializing the LCD
-  lcd.begin(0, lcdNumCols);
-  lcd.clear();
+int readings[numReadings]; // the readings from the analog input
+int readIndex = 0;         // the index of the current reading
+int total = 0;             // the running total
+int average = 0;           // the average
 
-  // clear smoothing array
-  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
-    readings[thisReading] = 0;
-  }
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);   // -- creating LCD instance
+LcdBarGraph lbg(&lcd, lcdNumCols, 0, 1); // -- creating bargraph instance, format is (&lcd, lcdNumCols, start X, start Y). So (&lcd, 16, 0, 1) would set the bargraph length to 16 columns and start the bargraph at column 0 on row 1.
+
+void setup()
+{
+    Serial.begin(9600);
+
+    // initializing the LCD
+    lcd.begin(0, lcdNumCols);
+    lcd.clear();
+
+    // clear smoothing array
+    for (int thisReading = 0; thisReading < numReadings; thisReading++)
+    {
+        readings[thisReading] = 0;
+    }
 }
 
-double retrieveSendingUnitValue(){
-  // here's where the sending unit analog input will be read
-  return analogRead(2); 
+double retrieveSendingUnitValue()
+{
+    // here's where the sending unit analog input will be read
+    return analogRead(2);
 }
 
-int derivePercentage(double rawValue){
-  // say min value is 100 and max value is 2000 (TODO: UPDATE WITH ACTUAL VALUES FROM THE SENDING UNIT)
-  const double lowestRawValue = 50;
-  const double highestRawValue = 2000;
-  const double zeroPercent = 0;
-  const double oneHundredPercent = 100;
-  return map(rawValue, lowestRawValue, highestRawValue, zeroPercent, oneHundredPercent); 
+int derivePercentage(double rawValue)
+{
+    // say min value is 100 and max value is 2000 (TODO: UPDATE WITH ACTUAL VALUES FROM THE SENDING UNIT)
+    const double lowestRawValue = 50;
+    const double highestRawValue = 2000;
+    const double zeroPercent = 0;
+    const double oneHundredPercent = 100;
+    return map(rawValue, lowestRawValue, highestRawValue, zeroPercent, oneHundredPercent);
 }
 
-int smoothPercentage(int rawPercentage){
-  // subtract the last reading:
-  total = total - readings[readIndex];
-  // add new reading input to array:
-  readings[readIndex] = rawPercentage;
-  // add the reading to the total:
-  total = total + readings[readIndex];
-  // advance to the next position in the array:
-  readIndex = readIndex + 1;
+int smoothPercentage(int rawPercentage)
+{
+    // subtract the last reading:
+    total = total - readings[readIndex];
+    // add new reading input to array:
+    readings[readIndex] = rawPercentage;
+    // add the reading to the total:
+    total = total + readings[readIndex];
+    // advance to the next position in the array:
+    readIndex = readIndex + 1;
 
-  // if we're at the end of the array...
-  if (readIndex >= numReadings) {
-    // ...wrap around to the beginning:
-    readIndex = 0;
-  }
+    // if we're at the end of the array...
+    if (readIndex >= numReadings)
+    {
+        // ...wrap around to the beginning:
+        readIndex = 0;
+    }
 
-  // calculate the average:
-  average = total / numReadings;
+    // calculate the average:
+    average = total / numReadings;
 
-  return average;
+    return average;
 }
- 
+
 void loop()
 {
-  rawSendingUnitValue = retrieveSendingUnitValue();
-  rawPercentage = derivePercentage(rawSendingUnitValue);
-  smoothedPercentage = smoothPercentage(rawPercentage);
-  
-  // numeric display option
-  lcd.setCursor(0,0);
-  lcd.print("Percentage: ");
-  lcd.print(smoothedPercentage);
-  lcd.print("%");
+    rawSendingUnitValue = retrieveSendingUnitValue();
+    rawPercentage = derivePercentage(rawSendingUnitValue);
+    smoothedPercentage = smoothPercentage(rawPercentage);
 
-  // bar chart option
-  lbg.drawValue(smoothedPercentage, 100);
+    // numeric display option
+    lcd.setCursor(0, 0);
+    lcd.print("Percentage: ");
+    lcd.print(smoothedPercentage);
+    lcd.print("%");
 
-  delay(100);
+    // bar chart option
+    lbg.drawValue(smoothedPercentage, 100);
+
+    delay(100);
 }
